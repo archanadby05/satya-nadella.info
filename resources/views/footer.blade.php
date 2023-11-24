91237bde91e7ab15d1c17bec1337be15b714b8de
archanadby05 <archanadby05@gmail.com>
2023-11-24 23:45:41 +0530

    Added Header and footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer Example</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        footer {
+            background-color: #333;
+            color: white;
+            padding: 20px;
+            text-align: center;
+        }
+
+        .footer-section {
+            margin-bottom: 20px;
+        }
+
+        .footer-section h3 {
+            margin-bottom: 10px;
+        }
+
+        .footer-section ul {
+            list-style-type: none;
+            padding: 0;
+        }
+
+        .footer-section li {
+            margin-bottom: 5px;
+        }
+
+        .footer-section a {
+            color: white;
+            text-decoration: none;
+        }
+
+        .footer-section a:hover {
+            text-decoration: underline;
+        }
+
+        .copyright {
+            margin-top: 20px;
+            font-size: 14px;
+        }
+    </style>
+</head>
+<body>
+
+<!-- Content goes here -->
+
+<footer>
+    <div class="footer-section">
+        <h3>Contact Us</h3>
+        <p>123 Street, Cityville, Country</p>
+        <p>Email: info@example.com</p>
+        <p>Phone: +1 (555) 123-4567</p>
+    </div>
+
+    <div class="footer-section">
+        <h3>Quick Links</h3>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Services</a></li>
+        </ul>
+    </div>
+
+    <div class="copyright">
+        <p>&copy; 2023 Your Company. All rights reserved.</p>
+    </div>
+</footer>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('footer');
 });
 Route::get('/about', function () {
     return view('about');
